5beaffa143aa159414a92e5a541f2272396649ba
Sahil Abrar <zeroxkhan2@gmail.com>
2024-01-02 20:32:38 +0600

    added banner gif and text

--- contact_app_client/src/Components/Pages/Banner/Banner.jsx
+++ contact_app_client/src/Components/Pages/Banner/Banner.jsx
@@ -1,11 +1,20 @@
 const Banner = () => {
   return (
     <div>
-      <div>
-        text here
-      </div>
-      <div>
-        
+      <div className="flex flex-col md:flex-row lg:flex-row mx-auto">
+        <div>
+          <h1 className="font-bold">
+            Effortlessly organize and streamline your contacts with our
+            intuitive contact management platform
+          </h1>
+        </div>
+        <div>
+          <img
+            className="lg:w-[600px] lg:h-[600px] md:w-[400px] md:h-[400px] w-[300px] h-[300px]"
+            src="https://finwave.co/images/gif/Contact-1.gif"
+            alt=""
+          />
+        </div>
       </div>
     </div>
   );
